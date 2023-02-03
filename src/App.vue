acb91ff0e93c0bd8cda3925096af0bb8aba7f9c2
CamHaoHa <camhaovn@gmail.com>
2023-02-03 23:14:25 +1100

    add and style header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import {  RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
+
 </script>
 
 <template>
 
-
+  <TodoHeader />
   <RouterView />
 </template>
 
